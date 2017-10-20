fedc58f2ead1ae1b341c7bd54040399fc6199b89
elektrovolk <elektro-volk@yandex.ru>
2017-10-20 10:53:06 +0800

    Add render start

---
+++ EGGIN/render.cpp
@@ -0,0 +1,38 @@
+#include "console.h"
+#include "libwork.h"
+#include "host.h"
+#include "api.h"
+#include "render.h"
+
+static lib::instance render_lib;
+typedef void(*renderMain_t)(engineapi& api, render::api_s& render_api);
+static renderMain_t Render_Main;
+
+void render::init()
+{
+	// TODO
+}
+
+void render::start()
+{
+	console::dev("Loading render engine");
+	if ((render_lib = lib::open("render/render_opengl.dll")) == NULL)
+		host::criticalError("Unable to load the render engine");
+
+	if ((Render_Main = (renderMain_t)lib::addr(render_lib, "Ren_Main")) == NULL)
+		host::criticalError("Incorrect render library");
+	console::dev("...\n");
+
+	Render_Main(api, render::rApi);
+	render::rApi.main.createWindow("EGGIN", 800, 600, false);
+}
+
+void render::frame()
+{
+	// TODO
+}
+
+void render::shutdown()
+{
+	// TODO
+}
--- EGGIN/render.h
+++ EGGIN/render.h
@@ -2,6 +2,14 @@
 
 namespace render
 {
+	struct api_s {
+		struct {
+			void(*createWindow)(const char *name, int w, int h, bool fullscreen);
+		} main;
+	};
+
+	extern api_s rApi;
+
 	void init();
 	void start();
 	void frame();
